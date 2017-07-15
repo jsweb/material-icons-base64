a1decbd24afbbf6c5b85d360fd1fe61cce4bd9f7
Alex Bruno Cáceres <git.alexbr@outlook.com>
2017-07-15 19:13:45 -0400

    Build refactored and JSON list added

--- src/build.js
+++ src/build.js
@@ -2,11 +2,22 @@ const fs = require('fs'),
     path = require('path'),
     proc = require('child_process'),
     src = path.join(__dirname, '..', 'snipacks', 'web', 'src.css'),
-    dest = path.join(__dirname, 'index.less')
+    icn = path.join(__dirname, '..', 'snipacks', 'unpkg', 'icons.txt'),
+    less = path.join(__dirname, 'index.less'),
+    json = path.join(__dirname, '..', 'index.json'),
+    css = fs.readFileSync(src, 'utf8'),
+    txt = fs.readFileSync(icn, 'utf8'),
+    result = css.replace('truetype', 'woff')
+        .replace(/url.+ttf\)/, 'data-uri("../snipacks/unpkg/font.woff")'),
+    list = txt.split('\n').map(str => {
+        const [id, unicode] = str.split(' '),
+            filter = id.split('_')
+        return {
+            id, unicode, filter,
+            name: filter.join(' ')
+        }
+    })
 
-fs.readFile(src, 'utf8', (e, str) => {
-    const result = str
-        .replace('truetype', 'woff')
-        .replace(/url.+ttf\)/, 'data-uri("../snipacks/unpkg/font.woff")')
-    fs.writeFile(dest, result, () => proc.exec('yarn start'))
+fs.writeFile(less, result, () => {
+    fs.writeFile(json, JSON.stringify(list), () => proc.exec('yarn start'))
 })
