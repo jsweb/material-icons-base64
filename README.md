56e8623d0729d4ce7e9d503e2dfc5d467b69d46e
Alex Bruno Cáceres <alxbr@alxbr-acer-w10.localdomain>
2020-11-18 10:17:57 -0300

    Change build compiler, using LESS

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Material Icons compiled with base64 embedded font to CSS instead of link to font
 
 ## About
 
-This compilation uses original files from Google and a little custom hack with Stylus build to replace `@font-face src url` settings and embed font with `data-uri`.
+This compilation uses original files from Google and a little custom build hack with LESS to replace `@font-face src url` settings and embed font with `data-uri`.
 
-Only WOFF2 fonts are included to minify the compiled CSS at most as possible and because WOFF2 is supported by all major modern browsers.
+Only WOFF2 fonts are included to minify the compiled CSS at most as possible and because WOFF2 is supported by all modern major browsers. The result CSS file is about 125kb, with no compression.
 
 Web projects are the target. Specially PWA with offline capabilities, where request remote Google files to get the icons couldn't work.
 
@@ -32,7 +32,7 @@ IE compatibility is not a concern. For a compatibility reference see the link:
 
 ## Files
 
-If you download the package using NPM or Yarn, you need to use `main.css` file.
+If you download the package using NPM or Yarn, you need to use `main.css` file, at the root package folder.
 
 You can see the icons at Material Icons index:
 
--- build.js
+++ build.js
@@ -1,38 +1,35 @@
 import { join } from 'path'
 import { readFileSync, writeFileSync } from 'fs'
-import stylus from 'stylus'
+import less from 'less'
 import pack from './package.json'
 
 const root = process.cwd()
 const now = new Date().toJSON()
 
 const main = join(root, 'main.css')
-const styl = join(root, 'jsweb-packs', 'web', 'source.styl')
-const source = readFileSync(styl, 'utf8')
+const file = join(root, 'jsweb-packs', 'web', 'source.css')
+const source = readFileSync(file, 'utf8')
 
-const result = source
+const css = source
   .replace('truetype', 'woff2')
-  .replace(/https.+.ttf/, 'source.woff2')
+  .replace(/https.+.ttf/, 'source.woff2"')
+  .replace('url(', 'data-uri("')
 
-stylus(result)
-  .set('filename', styl)
-  .define('url', stylus.url({ limit: false }))
-  .render((err, css) => {
-    if (err) {
-      console.log('Error:', err)
-    } else {
-      const result = [
-        '/**',
-        ` * ${pack.name}`,
-        ` * @version ${pack.version}`,
-        ` * @desc ${pack.description}`,
-        ` * @author ${pack.author}`,
-        ' * @create date 2017-07-15 14:50:00',
-        ` * @modify date ${now}`,
-        ' */',
-        css,
-      ].join('\n')
+less
+  .render(css, { filename: file })
+  .then((output) => {
+    const result = [
+      '/**',
+      ` * ${pack.name}`,
+      ` * @version ${pack.version}`,
+      ` * @desc ${pack.description}`,
+      ` * @author ${pack.author}`,
+      ' * @create date 2017-07-15 14:50:00',
+      ` * @modify date ${now}`,
+      ' */',
+      output.css,
+    ].join('\n')
 
-      writeFileSync(main, result)
-    }
+    writeFileSync(main, result)
   })
+  .catch((error) => console.log('Error:', error))
